0ddcfc36f9aa06c8698d24b5c9fd05692af67b59
uduwela96 <puduwel@mitrai.com>
2023-07-21 15:34:09 +0530

    Product microservice created

---
+++ Shopify.Product.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shopify.Product.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
