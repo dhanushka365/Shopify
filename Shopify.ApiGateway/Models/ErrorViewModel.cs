ebb613424639a52a0e65774e3a10bbfd59700c47
uduwela96 <puduwel@mitrai.com>
2023-07-21 13:26:55 +0530

    Add project files.

---
+++ Shopify.ApiGateway/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shopify.ApiGateway.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
