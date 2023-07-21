9d4cef7bd5e3856e103d8a0da23108e01e136fb9
uduwela96 <puduwel@mitrai.com>
2023-07-21 15:36:31 +0530

    Order microservice created

---
+++ Shopify.Order.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shopify.Order.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
